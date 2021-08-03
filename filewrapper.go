31a7ab1bdbf359d8f54bbbe02d72511655319b10
Vinz Frauchiger <vinzenz@frahof.net>
2021-08-03 17:42:11 +0200

    png commit!

---
+++ filewrapper.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/cavaliercoder/grab"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+type Document struct {
+	ApplId    string `json:"applicationNumberText"`
+	MRDate    string `json:"mailRoomDate"`
+	DocCode   string `json:"documentCode"`
+	DocDesc   string `json:"documentDescription"`
+	DocCate   string `json:"documentCategory"`
+	AccessLev string `json:"accessLevelCategory"`
+	DocIdent  string `json:"documentIdentifier"`
+	PagCount  int    `json:"pageCount"`
+	PdfURL    string `json:"pdfUrl"`
+}
+
+func GetFileWrapper(applId string) error {
+	// cms link
+	url_down := "https://ped.uspto.gov/api/queries/cms/"
+	// base url for file list
+	url_list_files := "https://ped.uspto.gov/api/queries/cms/public/"
+	comb_url_list := url_list_files + applId
+	fmt.Println(comb_url_list)
+
+	res, err := http.Get(comb_url_list)
+	if err != nil {
+
+		return err
+	}
+	defer res.Body.Close()
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+
+		return err
+	}
+
+	var documents []Document
+	json.Unmarshal(body, &documents)
+
+	//iterate through every document and download it into the present folder
+	for i := 0; i < len(documents); i++ {
+		fmt.Println("Application Id: " + documents[i].ApplId)
+		fmt.Println("Document Type: " + documents[i].DocCate)
+		fmt.Println("Mail Room Date: " + documents[i].MRDate)
+		t, _ := time.Parse("01-02-2006", documents[i].MRDate)
+		fmt.Println(t)
+		fmt.Println("Document Identifier: " + documents[i].DocIdent)
+		fmt.Println("pdfUrl: " + documents[i].PdfURL)
+		doc_url := url_down + documents[i].PdfURL
+		if documents[i].PdfURL == "" {
+			fmt.Println("THERE IS NO URL FOR THE FILE")
+			continue
+		}
+		resp, err := grab.Get(".", doc_url)
+		if err != nil {
+			return err
+		}
+		// define, refine, and rename the file name of the downloaded files, less cryptic
+		filename := documents[i].ApplId + "_" + documents[i].MRDate + "_" + documents[i].DocDesc + ".pdf"
+		filename = strings.ReplaceAll(filename, "/", "_")
+		filename = strings.ReplaceAll(filename, ":", "_")
+		os.Rename(resp.Filename, filename)
+
+		fmt.Println("Download saved to", resp.Filename, filename)
+	}
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	canvas2 "fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"log"
+	"strings"
+)
+
+func modifyText(rawText string) string {
+	if strings.ToUpper(rawText[:2]) == "US" {
+		rawText = rawText[2:]
+	}
+	l := len(rawText)
+	if strings.ToUpper(string(rawText[l-2])) == "A" || strings.ToUpper(string(rawText[l-2])) == "B" {
+		rawText = rawText[:l-2]
+	}
+	return rawText
+}
+
+func removeChars(rawText string) string {
+	rawText = strings.ReplaceAll(rawText, "/", "")
+	rawText = strings.ReplaceAll(rawText, "-", "")
+	rawText = strings.ReplaceAll(rawText, " ", "")
+	return rawText
+}
+
+var theApplId2 string = ""
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("USPTO PEDS Tool Go!")
+	hello := widget.NewLabel("Hello Dude!")
+
+	labApplId := widget.NewLabel("Application ID")
+	inpApplId := widget.NewEntry()
+	inpApplId.SetPlaceHolder("12321123")
+
+	labPatentNum := widget.NewLabel("Patent Number")
+	inpPatentNum := widget.NewEntry()
+	inpPatentNum.SetPlaceHolder("7123456 or 11321123")
+
+	labEarlPubNum := widget.NewLabel("Early Publication Number")
+	inpEarlPubNum := widget.NewEntry()
+	inpEarlPubNum.SetPlaceHolder("Us20080123456A1")
+
+	// images
+	image := canvas2.NewImageFromFile("gopherli.png")
+	image.FillMode = canvas2.ImageFillOriginal
+	imageYps := canvas2.NewImageFromFile("Ypsomed_big.png")
+	imageYps.FillMode = canvas2.ImageFillOriginal
+
+	labTitle := widget.NewLabelWithStyle(
+		"GopherPEDS",
+		fyne.TextAlignCenter,
+		fyne.TextStyle{Bold: true},
+	)
+	labCopyRight := widget.NewLabelWithStyle(
+		"© Vinz Frauchiger, 2021",
+		fyne.TextAlignCenter,
+		fyne.TextStyle{Bold: true},
+	)
+	labExclPatents := widget.NewLabelWithStyle(
+		" Proudly for Ypsomed Patents!",
+		fyne.TextAlignCenter,
+		fyne.TextStyle{Italic: true},
+	)
+
+	butTermApplID := widget.NewButton("Go Appl Id", func() {
+		modifiedText := modifyText(inpApplId.Text)
+		modifiedText = removeChars(modifiedText)
+		termdays, discl, _, err := GetTermDisc("applId", modifiedText)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if discl == "" {
+			discl = "No terminal disclaimer found!"
+		}
+		hello.SetText(termdays + " days and " + discl)
+	})
+	butWrapApplId := widget.NewButton("Get FileWrapper", func() {
+		hello.SetText(theApplId2)
+	})
+
+	// Buttons Patents
+	butPatNumTerm := widget.NewButton("Go Pat Num", func() {
+		modifiedText := modifyText(inpPatentNum.Text)
+		modifiedText = removeChars(modifiedText)
+		hello.SetText(modifiedText)
+		termdays, discl, theApplId, err := GetTermDisc("patentNumber", modifiedText)
+		if err != nil {
+			log.Fatal(err)
+		}
+		theApplId2 = theApplId
+		if discl == "" {
+			discl = "No terminal disclaimer found!"
+		}
+		hello.SetText(termdays + " days and " + discl + " /ApplID " + theApplId)
+	})
+	butPatNumWrap := widget.NewButton("Get FileWrapper for Patent", func() {
+		modifiedText := modifyText(inpPatentNum.Text)
+		modifiedText = removeChars(modifiedText)
+		hello.SetText(modifiedText)
+		_, _, theApplId, err := GetTermDisc("patentNumber", modifiedText)
+		if err != nil {
+			hello.SetText("wrong number format!")
+		} else if theApplId == "number!" {
+			hello.SetText("wrong number!(kind code?)")
+		} else {
+			theApplId2 = theApplId
+			hello.SetText("Getting FileWrapper for " + theApplId2)
+			err = GetFileWrapper(theApplId2)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	})
+
+	// Buttons Early Publication
+	butEarlPubNumTerm := widget.NewButton("Go Publ Num", func() {
+		modifiedText := removeChars(inpEarlPubNum.Text)
+		modifiedText = strings.ToUpper(modifiedText)
+		hello.SetText(modifiedText)
+		termdays, discl, theApplId, err := GetTermDisc("appEarlyPubNumber", modifiedText)
+		if err != nil {
+			fmt.Println(termdays, discl, theApplId, err)
+		}
+		if discl == "" {
+			discl = "No terminal disclaimer found!"
+		}
+		if termdays == "" {
+			termdays = "not granted yet!"
+		}
+		hello.SetText(termdays + " days and " + discl + " / " + theApplId)
+	})
+	butEarlPubNumWrap := widget.NewButton("Get FileWrapper Publ", func() {
+		modifiedText := removeChars(inpEarlPubNum.Text)
+		modifiedText = strings.ToUpper(modifiedText)
+		hello.SetText(modifiedText)
+		_, _, theApplId, err := GetTermDisc("appEarlyPubNumber", modifiedText)
+		if err != nil {
+			hello.SetText("wrong number format!")
+		} else if theApplId == "number!" {
+			hello.SetText("wrong number!(kind code?)")
+		} else {
+			theApplId2 = theApplId
+			hello.SetText("Getting FileWrapper for " + theApplId2)
+			err = GetFileWrapper(theApplId2)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	})
+
+	// CONTENT
+	content := container.NewVBox(
+		container.NewHBox(
+			image,
+			container.NewVBox(
+				labTitle,
+				imageYps,
+				labCopyRight,
+				labExclPatents,
+			),
+		),
+		widget.NewSeparator(),
+		widget.NewLabel("Find Output below:"),
+		widget.NewSeparator(),
+		hello,
+		widget.NewSeparator(),
+		labApplId,
+		inpApplId,
+		container.NewHBox(
+			butTermApplID,
+			butWrapApplId,
+		),
+		labPatentNum,
+		inpPatentNum,
+		container.NewHBox(
+			butPatNumTerm,
+			butPatNumWrap,
+		),
+		labEarlPubNum,
+		inpEarlPubNum,
+		container.NewHBox(
+			butEarlPubNumTerm,
+			butEarlPubNumWrap,
+		),
+		widget.NewButton("Quit", w.Close))
+	w.SetContent(content)
+	w.Resize(fyne.NewSize(600, 400))
+	w.ShowAndRun()
+}
